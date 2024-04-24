b3ecc73405eeb96d049a8dfdc57439614e2cffcb
RavindhiranG <ravindhirang@gmail.com>
2024-04-24 16:53:55 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pipes';
-}
+
+  today: number = Date.now();
+
+
+
+  name = "";
+
+  dateOfBirth = "";
+
+  //currency Convertor
+
+  usdAmount = 0;
+  inrAmount = 0;
+  exchangeRate = 83.31; // exchange rate for demonstration
+
+  convertToINR() {
+    this.inrAmount = this.usdAmount * this.exchangeRate;
+  }
+
+  convertToUSD() {
+    this.usdAmount = this.inrAmount / this.exchangeRate;
+  }
+
+  
+
+
+ }
